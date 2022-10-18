a19df194063f4f817293e5e95cb3b8d4448c859e
Deonniss <dengolovin2017@gmail.com>
2022-10-18 22:44:23 +0700

    replace returned param

--- src/main/java/ru/tusur/server/util/DrmHandler.java
+++ src/main/java/ru/tusur/server/util/DrmHandler.java
@@ -10,7 +10,6 @@ import java.util.Set;
 
 public class DrmHandler {
 
-
     private static final Map<String, DrmData> usersData;
     private static final Map<String, Boolean> licenseData;
     private static final Set<String> hardwareSet;
@@ -30,69 +29,68 @@ public class DrmHandler {
         licenseData.put("BBBB-XXXX-XXXX-XXXX", false);
     }
 
-    public static StatusCode isCorrectPassword(String username, String password) {
+    public static int isCorrectPassword(String username, String password) {
         if (!isExist(username)) {
-            return StatusCode.LOGIN_FAILED_201;
+            return StatusCode.LOGIN_FAILED_201.code;
         } else if (!usersData.get(username).getPassword().equals(password)) {
-            return StatusCode.LOGIN_FAILED_202;
+            return StatusCode.LOGIN_FAILED_202.code;
         } else {
-            return StatusCode.LOGIN_SUCCESS_101;
+            return StatusCode.LOGIN_SUCCESS_101.code;
         }
     }
 
-    public static StatusCode addUser(String username, String password) {
+    public static int addUser(String username, String password) {
         if (!isExist(username)) {
             DrmData drmData = new DrmData();
             drmData.setPassword(password);
             usersData.put(username, drmData);
-            return StatusCode.REGISTRATION_SUCCESS_110;
+            return StatusCode.REGISTRATION_SUCCESS_110.code;
         }
-        return StatusCode.REGISTRATION_FAILED_210;
+        return StatusCode.REGISTRATION_FAILED_210.code;
     }
 
-    public static StatusCode checkHardware(String username, String hardware) {
+    public static int checkHardware(String username, String hardware) {
         if (!isExist(username)) {
-            return StatusCode.LOGIN_FAILED_201;
+            return StatusCode.LOGIN_FAILED_201.code;
         } else if (!usersData.get(username).getHardware().equals(hardware)) {
-            return StatusCode.HARDWARE_FAILED_230;
+            return StatusCode.HARDWARE_FAILED_230.code;
         } else {
-            return StatusCode.HARDWARE_SUCCESS_130;
+            return StatusCode.HARDWARE_SUCCESS_130.code;
         }
     }
 
-    public static StatusCode addHardware(String username, String hardware) {
+    public static int addHardware(String username, String hardware) {
 
         if (!isExist(username)) {
-            return StatusCode.LOGIN_FAILED_201;
+            return StatusCode.LOGIN_FAILED_201.code;
         } else if (hardwareSet.contains(hardware)) {
-            return StatusCode.HARDWARE_FAILED_231;
+            return StatusCode.HARDWARE_FAILED_231.code;
         } else {
             hardwareSet.add(hardware);
             DrmData drmData = usersData.get(username);
             drmData.setHardware(hardware);
             usersData.put(username, drmData);
-            return StatusCode.HARDWARE_SUCCESS_130;
+            return StatusCode.HARDWARE_SUCCESS_130.code;
         }
     }
 
-    public static StatusCode addLicense(String username, String key) {
+    public static int addLicense(String username, String key) {
         if (!isExist(username)) {
-            return StatusCode.LOGIN_FAILED_201;
+            return StatusCode.LOGIN_FAILED_201.code;
         } else if (!licenseData.containsKey(key)) {
-            return StatusCode.LICENSE_FAILED_221;
+            return StatusCode.LICENSE_FAILED_221.code;
         } else if (licenseData.get(key)) {
-            return StatusCode.LICENSE_FAILED_220;
+            return StatusCode.LICENSE_FAILED_220.code;
         } else {
             licenseData.put(key, !licenseData.get(key));
             DrmData drmData = usersData.get(username);
             drmData.setLicense(key);
             usersData.put(username, drmData);
-            return StatusCode.LICENSE_SUCCESS_120;
+            return StatusCode.LICENSE_SUCCESS_120.code;
         }
     }
 
     private static boolean isExist(String username) {
         return usersData.containsKey(username);
     }
-
 }
